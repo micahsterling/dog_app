6dd2cf73eda5d6b9cb5865266e9a36f3a65722d3
Micah Sterling <msterling89@gmail.com>
2021-02-27 10:10:51 -0600

    added dog index

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,2 +1,6 @@
 class Api::DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+    render 'index.json.jb'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# dog = Dog.new(name: "Peanut", breed: "Terrier mix", age: 1)
+# dog.save
+# dog = Dog.new(name: "Coach", breed: "Lab", age: 0.5)
+# dog.save
+# dog = Dog.new(name: "Levine", breed: "Beagle", age: 2)
+# dog.save
+# dog = Dog.new(name: "Gaston", breed: "Beagle", age: 3)
+# dog.save
+# dog = Dog.new(name: "Cookie", breed: "Husky mix", age: 3)
+# dog.save
